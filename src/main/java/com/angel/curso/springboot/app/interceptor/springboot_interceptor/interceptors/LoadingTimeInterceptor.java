702ad8d55237b61c7614d4e4591eb496fe9fb7a1
Angel Ramirez <aramirezd@gmail.com>
2025-05-28 11:09:18 -0600

    Clase MvcConfig preparada con metodos en Clase LoadingTimerIntercepetor preparados

--- src/main/java/com/angel/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/angel/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.angel.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,18 +9,20 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class); 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
+                logger.info("LayerInstantiationException: postHandle() saliendo... ");
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-
+                logger.info("LayerInstantiationException: preHandle() entrando... ");
         return true;
     }
 
